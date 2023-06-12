59abb6661bf9c74e0fc102b047f08eb013cac7e4
meghub <meghub@proton.me>
2023-06-12 14:50:20 +0200

    counting mechanic working

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,32 @@
-import express from 'express ';
+import express from 'express';
 import mongoose from 'mongoose';
 
-const PORT = process.env.PORT || 5172;
+import 'dotenv/config.js'
 
 const app = express();
-app.use(express.json)
+const PORT = process.env.PORT || 3002;
+//git commit but not pushed 1
+
+app.use(express.json());
+mongoose.connect(process.env.DB)
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl} ${res.statusCode}`)
+    next()
+})
+
+let count = 0;
+app.get('/visit', (req, res) => {
+    if (res.sendStatus(200)) {
+        return count += 1;
+    }
+})
+console.log(count);
+app.get('/visited', (req, res) => {
+    res.send(`${count}`)
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 });
+
