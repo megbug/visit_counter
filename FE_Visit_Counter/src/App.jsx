7d107e09cadfab7a685dae7a69ec90772d698017
meghub <meghub@proton.me>
2023-06-14 07:33:28 +0200

    finished page to look up visitor numbers

--- FE_Visit_Counter/src/App.jsx
+++ FE_Visit_Counter/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+import axios from 'axios';
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [counts, setCounts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5172/visited')
+      .then(({ data }) => setCounts(data))
+    const id = setInterval(() => { }, 200)
+    return () => {
+      clearInterval(id);
+    }
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Schön, dass Sie hier sind</p>
+      <h1>Counter</h1>
+      {counts.map((item) => {
+        return (
+          <p>{item.name} : {item.count}</p>
+        )
+      })}
     </>
   )
 }
