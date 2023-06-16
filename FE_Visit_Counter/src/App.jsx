ab173cdf007a90ecf27521f3b2dd899749c69e7c
meghub <meghub@proton.me>
2023-06-16 20:31:40 +0200

    added comments

--- FE_Visit_Counter/src/App.jsx
+++ FE_Visit_Counter/src/App.jsx
@@ -5,41 +5,31 @@ import axios from 'axios';
 
 function App() {
 
-  const [counts, setCounts] = useState([]);
+    const [counts, setCounts] = useState([]);
 
-    ({ data }) => setCounts(data);
-    (res) => {
-        let data = res.data;
-        return setCounts(data);
+    useEffect(() => {
+        const id = setInterval(() => {                      // with setInterval the counts get delayed for 200ms 
+            axios.get('http://localhost:5172/visited')      // axios gets data from api 
+                .then((res) => { setCounts(res.data) });    // data is what's get send from the server.js in res.send
+        }, 200)
+        return () => {
+            clearInterval(id);
+        }
+    }, [counts])
 
-    }
-    res => {
-        let { data } = res;
-        return setCounts(data)
-    }
-
-
-  useEffect(() => {
-    axios.get('http://localhost:5172/visited')
-        // axios.get('http://localhost:5172/visited')
-        //     .then(({ data }) => setCounts(data))
-    const id = setInterval(() => { }, 200)
-    return () => {
-      clearInterval(id);
-    }
-  }, [])
-
-  return (
-    <>
-      <p>Schön, dass Sie hier sind</p>
-      <h1>Counter</h1>
-      {counts.map((item) => {
-        return (
-          <p>{item.name} : {item.count}</p>
-        )
-      })}
-    </>
-  )
+    return (
+        <>
+            <p>Schön, dass Sie hier sind</p>
+            <h1>Counter</h1>
+            {counts.map((item) => {
+                return (
+                    <p>{item.name} : {item.count}</p>
+                )
+            })}
+        </>
+    )
 }
 
 export default App
+
+    * // setInterval verzögert den request um 200 ms, damit nur alle 200 ms refreshed wird in Verbindung mit dependency array 
