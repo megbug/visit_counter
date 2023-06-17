18b9c1324427454eb075185b308da026a888a2f4
meghub <meghub@proton.me>
2023-06-17 12:44:51 +0200

    commented alternative syntax

--- Server/server.js
+++ Server/server.js
@@ -29,16 +29,42 @@ app.get('/visit', async (req, res) => {
     let counts = await Count.findOne({ name: `${site}` });
     //console.log(counts); //logs Obj with : _id, name of site, counted number in case site was used before; else logs: null
 
+    // let timeVisit
+    // let browserAgent
+    // let ipAddresses
+
     if (counts == null) {
         // in case the site requested has no counts yet, we create it in the database
-        const startCount = await Count.create({ name: `${site}`, count: 1 });
+        // const startCount = await Count.create({ name: `${site}`, count: 1 });
+        // timeVisit = new Date();
+        // browserAgent = req.get('user-agent');
+        // ipAddresses = req.socket.remoteAddress;
+        // console.log(ipAddresses);
+        //console.log(timeVisited.toString());
+        const startCount = await Count.create({
+            name: `${site}`,
+            count: 1,
+            clientInfo: {
+                // lastUserIP: ipAddresses,
+                lastUserIP: req.socket.remoteAddress,
+                // lastVisit: timeVisit.toString(),
+                lastVisit: new Date().toString(),
+                lastBrowserAgent: req.get('user-agent')
+                // lastBrowserAgent: browserAgent
+            }
+        });
         res.send(`Visiters: ${startCount.count}`)
         // the response we send to the client using the api endpoint (/visit?site=something)
     }
     else {
         // in case the site requested already has counts we're adding the new counts and save them to the database 
+        let timeVisit = new Date();
         counts.count += 1;
+        counts.clientInfo.lastUserIP = req.socket.remoteAddress;
+        counts.clientInfo.lastVisit = timeVisit.toString();
+        counts.clientInfo.lastBrowserAgent = req.get('user-agent');
         counts.save()
+
         res.send(`Visiters: ${counts.count}`)
     }
 });
@@ -53,3 +79,15 @@ app.get('/visited', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 });
+
+// console.log((req.get('user-agent'))
+// new date 
+/*
+app.get('/',function(req, res) {
+
+    const ipAddresses = req.header('x-forwarded-for');
+    res.send(ipAddresses);
+
+});
+*/
+
